5cd3672d60a53d62511c8150dceee8119542fd2f
Ahmed Radi <ahmedradi743@gmail.com>
2020-10-02 01:27:54 +0200

    Add Sections  courses table ,our team and add Font Awesome

--- js/plugins.js
+++ js/plugins.js
@@ -9,7 +9,6 @@ $(window).scroll(function(){
   const displayFeat = $('.feat') ;
   if($(window).scrollTop() >= 1100 ){
     displayFeat.fadeIn(600);
-    console.log('sdsad')
   }else{
     displayFeat.fadeOut(1000);
   }
