256397fee87b79f461d531720689df7e5c6a2f4b
Ahmed Radi <ahmedradi743@gmail.com>
2020-10-01 23:49:07 +0200

    Add Some Edit in Css file and add function to hide and appear feature

--- js/plugins.js
+++ js/plugins.js
@@ -3,3 +3,14 @@ $(document).ready(function(){
     interval: 4000
   });
 });
+
+
+$(window).scroll(function(){
+  const displayFeat = $('.feat') ;
+  if($(window).scrollTop() >= 1100 ){
+    displayFeat.fadeIn(600);
+    console.log('sdsad')
+  }else{
+    displayFeat.fadeOut(1000);
+  }
+});
