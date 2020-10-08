f4995b25d7551e26ac37a89cc8873dc36a276de5
Ahmed Radi <ahmedradi743@gmail.com>
2020-10-08 17:29:59 +0200

    Add JQuery Code to change color Using 'li'

--- js/plugins.js
+++ js/plugins.js
@@ -12,13 +12,28 @@ $(window).scroll(function(){
   }else{
     displayFeat.fadeOut(1000);
   }
-//**End hidden section Features & show when reach to 1000px */
+//****End hidden section Features & show when reach to 1000px ****/
 
 //**Start Show Color Option Div When Click on the Gear */
 $('.gear-check').click(function(){
   $('.color-option').toggle();
 });
 //**End Show Color Option Div When Click on the Gear */
+
+//**** Change Color theme on Click *****/
+var colorLi = $('.color-option ul li');
+colorLi
+  .eq(0).css('backgroundColor','#E41817').end()
+  .eq(1).css('backgroundColor','#0895D1').end()
+  .eq(2).css('backgroundColor','#0EE681').end()
+  .eq(3).css('backgroundColor','#F1E31B').end()
+  .eq(4).css('backgroundColor','#BE8CD1');
+
+  colorLi.click(function(){
+    $("link[href*='theme']").attr("href",$(this).attr("data-value"));
+
+    //console.log()
+  });
 
 
 });
